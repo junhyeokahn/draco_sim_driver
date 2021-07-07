8e58a05fdd1b4963ad01d1cd21c1d3f957c0fdcc
junhyeokahn <junhyeokahn91@gmail.com>
2021-07-07 18:14:52 -0500

    hook pnc lib

--- include/draco_sim_driver/draco_sim_driver.hpp
+++ include/draco_sim_driver/draco_sim_driver.hpp
@@ -4,12 +4,23 @@
 #include <cortex_data_plugins/generic_state_data.hpp>
 #include <cortex_framework/plugin_base/controller_base.hpp>
 
+#include <pnc/draco_pnc/draco_interface.hpp>
+
 namespace aptk {
 namespace ctrl {
 class DracoSimDriver : public ctrl::ControllerBase {
 public:
-  DracoSimDriver(){};
-  ~DracoSimDriver(){};
+  DracoSimDriver() {
+
+    draco_interface_ = new DracoInterface(true);
+    draco_sensor_data_ = new DracoSensorData();
+    draco_command_ = new DracoCommand();
+  };
+  ~DracoSimDriver() {
+    delete draco_interface_;
+    delete draco_sensor_data_;
+    delete draco_command_;
+  };
   void initialize(boost::shared_ptr<ctrl::DriverBase> driver,
                   boost::shared_ptr<ctrl::StateDataBase> state_data_base,
                   boost::shared_ptr<ctrl::CommandDataBase> command_data_base) {
@@ -24,6 +35,20 @@ public:
 
     command_data_base->initialize(driver_->rtNodeHandle(), driver_->logger());
     command_data_base->addDebugInterfaces();
+
+    for (int i = 0; i < skel_->getNumJoints(); ++i) {
+      dart::dynamics::JointPtr joint = skel_->getJoint(i);
+      if (joint->getName() == "rootJoint") {
+      } else if (joint->getType() != "WeldJoint") {
+        joint_id_[joint->getName()] = joint;
+      } else {
+      }
+    }
+
+    for (int i = 0; i < skel_->getNumBodyNodes(); ++i) {
+      dart::dynamics::BodyNodePtr bn = skel_->getBodyNode(i);
+      link_id_[bn->getName()] = bn;
+    }
   }
 
   // control loop
@@ -39,6 +64,13 @@ private:
 
   boost::shared_ptr<GenericStateData> data_;
   boost::shared_ptr<GenericCommandData> command_;
+
+  DracoInterface *draco_interface_;
+  DracoSensorData *draco_sensor_data_;
+  DracoCommand *draco_command_;
+
+  std::map<std::string, dart::dynamics::JointPtr> joint_id_;
+  std::map<std::string, dart::dynamics::BodyNodePtr> link_id_;
 };
 } // namespace ctrl
 } // namespace aptk
--- src/draco_sim_driver.cpp
+++ src/draco_sim_driver.cpp
@@ -3,20 +3,65 @@
 namespace aptk {
 namespace ctrl {
 void DracoSimDriver::updateCommandData() {
-
-  // print out joint positions
-  // std::cout << "pos dof" << std::endl;
-  // std::cout << data_->pos_dof[5] << std::endl;
-  // std::cout << data_->pos_dof.size() << std::endl;
-
-  // set zero torque
-  // std::cout << command_->pos_atv.size() << std::endl;
-  // std::cout << "-------" << std::endl;
+  // copy data
+  CopyData();
+  // compute command
+  draco_interface_->getCommand(draco_sensor_data_, draco_command_);
+  // coppy command
+  CopyCommand();
 }
 
-void DracoSimDriver::CopyData() {}
+void DracoSimDriver::CopyData() {
+  // copy imu data
+  Eigen::Isometry3d imu_iso = link_id_["torso_imu"]->getTransform(
+      dart::dynamics::Frame::World(), dart::dynamics::Frame::World());
+  draco_sensor_data_->imu_frame_iso.block(0, 0, 3, 3) = imu_iso.linear();
+  draco_sensor_data_->imu_frame_iso.block(0, 3, 3, 1) = imu_iso.translation();
+  draco_sensor_data_->imu_frame_vel = link_id_["torso_imu"]->getSpatialVelocity(
+      dart::dynamics::Frame::World(), dart::dynamics::Frame::World());
 
-void DracoSimDriver::CopyCommand() {}
+  // copy encoder data
+  for (std::map<std::string, dart::dynamics::JointPtr>::iterator it =
+           joint_id_.begin();
+       it != joint_id_.end(); it++) {
+    if (it->first == "l_knee_fe") {
+      draco_sensor_data_->joint_positions["l_knee_fe_jd"] =
+          joint_id_["l_knee_fe"]->getPosition(0) -
+          joint_id_["l_knee_fe_jp"]->getPosition(0);
+    } else if (it->first == "r_knee_fe") {
+      draco_sensor_data_->joint_positions["r_knee_fe_jd"] =
+          joint_id_["r_knee_fe"]->getPosition(0) -
+          joint_id_["r_knee_fe_jp"]->getPosition(0);
+    } else {
+      draco_sensor_data_->joint_positions[it->first] =
+          it->second->getPosition(0);
+    }
+  }
+}
+
+void DracoSimDriver::CopyCommand() {
+  // copy torques
+  for (std::map<std::string, dart::dynamics::JointPtr>::iterator it =
+           joint_id_.begin();
+       it != joint_id_.end(); it++) {
+    int idx = it->second->getIndexInSkeleton(0) - 6;
+    if (it->first == "l_knee_fe") {
+      // TODO : Double check this double up
+      command_->eff_atv[idx] =
+          2 * draco_command_->joint_torques["l_knee_fe_jd"];
+    } else if (it->first == "l_knee_fe_jp") {
+      // do nothing
+    } else if (it->first == "r_knee_fe") {
+      // TODO : Double check this double up
+      command_->eff_atv[idx] =
+          2 * draco_command_->joint_torques["r_knee_fe_jd"];
+    } else if (it->first == "r_knee_fe_jp") {
+      // do nothing
+    } else {
+      command_->eff_atv[idx] = draco_command_->joint_torques[it->first];
+    }
+  }
+}
 
 } // namespace ctrl
 } // namespace aptk
